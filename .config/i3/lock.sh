e44b08af3ca3b83b3250876aea969628b486c225
Asa Ayers <Asa@AsaAyers.com>
2017-10-21 22:44:24 -0700

    Switch to wallpaper as lock screen

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-lock_image=/tmp/screen_locked.png
-if [ "$HOSTNAME" == "asa-macbookpro" ]; then
-  lock_image=/tmp/screen_locked.png
-fi
+cd /home/asa/.config/i3
+lock_image=~/Pictures/wallpaper.jpg
 
-echo "lock.sh: $lock_image"
-time ~/.config/i3/screenshot.sh $lock_image
 # Synchronize cached writes to persistent storage
 sync
 
 # Lock screen displaying this image.
-i3lock --nofork --ignore-empty-password --image=$lock_image --show-failed-attempts
+./i3lock-multimonitor/lock -i $lock_image -a "--nofork --pointer=default --ignore-empty-password --show-failed-attempts"
